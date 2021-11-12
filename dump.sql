b94f9f002cc84770a7ea41f0759e8205bae1803e
bruch0 <lucas.bruch0@gmail.com>
2021-11-12 11:21:26 -0300

    feat: new dump, new files to ignore, new libs

--- dump.sql
+++ dump.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "users" (
-    "id" serial NOT NULL,
-    "name" varchar(255) NOT NULL,
-    "cpf" varchar(11) NOT NULL UNIQUE,
-    "email" varchar(100) NOT NULL UNIQUE,
-    "password" varchar(70) NOT NULL UNIQUE,
-    "phone" varchar(12) NOT NULL UNIQUE,
-    CONSTRAINT "users_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"cpf" varchar(11) NOT NULL UNIQUE,
+	"email" varchar(100) NOT NULL UNIQUE,
+	"password" varchar(70) NOT NULL UNIQUE,
+	"phone" varchar(12) NOT NULL UNIQUE,
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -13,13 +13,13 @@ CREATE TABLE "users" (
 
 
 CREATE TABLE "addresses" (
-    "id" serial NOT NULL UNIQUE,
-    "street" varchar(255) NOT NULL UNIQUE,
-    "user_id" integer NOT NULL,
-    "zip_code" varchar(8) NOT NULL,
-    "number" integer NOT NULL,
-    "complement" varchar(50) NOT NULL,
-    CONSTRAINT "addresses_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL UNIQUE,
+	"street" varchar(255) NOT NULL UNIQUE,
+	"user_id" integer NOT NULL,
+	"zip_code" varchar(8) NOT NULL,
+	"number" integer NOT NULL,
+	"complement" varchar(50) NOT NULL,
+	CONSTRAINT "addresses_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -27,18 +27,18 @@ CREATE TABLE "addresses" (
 
 
 CREATE TABLE "products" (
-    "id" serial NOT NULL,
-    "name" varchar(255) NOT NULL,
-    "category_id" integer NOT NULL,
-    "value" integer NOT NULL,
-    "description" varchar(1024) NOT NULL,
-    "total_qty" integer NOT NULL,
-    "weight" integer NOT NULL,
-    "brand_id" integer NOT NULL,
-    "model" varchar(255) NOT NULL,
-    "size" varchar(255) NOT NULL,
-    "color" varchar(255) NOT NULL,
-    CONSTRAINT "products_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"category_id" integer NOT NULL,
+	"value" integer NOT NULL,
+	"description" varchar(1024) NOT NULL,
+	"total_qty" integer NOT NULL,
+	"weight" integer NOT NULL,
+	"brand_id" integer NOT NULL,
+	"model" varchar(255) NOT NULL,
+	"size" varchar(255) NOT NULL,
+	"color" varchar(255) NOT NULL,
+	CONSTRAINT "products_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -46,9 +46,9 @@ CREATE TABLE "products" (
 
 
 CREATE TABLE "categories" (
-    "id" serial NOT NULL,
-    "name" varchar(255) NOT NULL,
-    CONSTRAINT "categories_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	CONSTRAINT "categories_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -56,10 +56,10 @@ CREATE TABLE "categories" (
 
 
 CREATE TABLE "products_images" (
-    "id" serial NOT NULL,
-    "product_id" serial NOT NULL,
-    "url" varchar(255) NOT NULL,
-    CONSTRAINT "products_images_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"product_id" serial NOT NULL,
+	"url" varchar(255) NOT NULL,
+	CONSTRAINT "products_images_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -67,10 +67,9 @@ CREATE TABLE "products_images" (
 
 
 CREATE TABLE "sessions" (
-    "id" serial NOT NULL,
-    "user_id" integer NOT NULL,
-    "token" varchar(36) NOT NULL,
-    CONSTRAINT "sessions_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	CONSTRAINT "sessions_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -78,11 +77,11 @@ CREATE TABLE "sessions" (
 
 
 CREATE TABLE "recovery_password" (
-    "id" serial NOT NULL,
-    "user_id" integer NOT NULL,
-    "token" varchar(36) NOT NULL,
-    "creation_date" TIMESTAMP NOT NULL DEFAULT 'now ()',
-    CONSTRAINT "recovery_password_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"token" varchar(36) NOT NULL,
+	"creation_date" integer NOT NULL,
+	CONSTRAINT "recovery_password_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -90,10 +89,10 @@ CREATE TABLE "recovery_password" (
 
 
 CREATE TABLE "purchases" (
-    "id" serial NOT NULL,
-    "user_id" integer NOT NULL,
-    "total_value" integer NOT NULL,
-    CONSTRAINT "purchases_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"total_value" integer NOT NULL,
+	CONSTRAINT "purchases_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -101,11 +100,11 @@ CREATE TABLE "purchases" (
 
 
 CREATE TABLE "bought_products" (
-    "id" serial NOT NULL,
-    "purchase_id" integer NOT NULL,
-    "product_id" integer NOT NULL,
-    "qty" integer NOT NULL,
-    CONSTRAINT "bought_products_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"purchase_id" integer NOT NULL,
+	"product_id" integer NOT NULL,
+	"qty" integer NOT NULL,
+	CONSTRAINT "bought_products_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -113,9 +112,9 @@ CREATE TABLE "bought_products" (
 
 
 CREATE TABLE "products_brands" (
-    "id" serial NOT NULL UNIQUE,
-    "name" varchar(255) NOT NULL UNIQUE,
-    CONSTRAINT "products_brands_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL UNIQUE,
+	CONSTRAINT "products_brands_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -123,10 +122,22 @@ CREATE TABLE "products_brands" (
 
 
 CREATE TABLE "product_contains" (
-    "id" serial NOT NULL,
-    "product_id" integer NOT NULL,
-    "item" varchar(255) NOT NULL,
-    CONSTRAINT "product_contains_pk" PRIMARY KEY ("id")
+	"id" serial NOT NULL,
+	"product_id" integer NOT NULL,
+	"item" varchar(255) NOT NULL,
+	CONSTRAINT "product_contains_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "cart" (
+	"id" serial NOT NULL,
+	"product_id" integer NOT NULL,
+	"user_id" integer NOT NULL,
+	"product_qty" integer NOT NULL,
+	CONSTRAINT "cart_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -153,3 +164,18 @@ ALTER TABLE "bought_products" ADD CONSTRAINT "bought_products_fk1" FOREIGN KEY (
 
 
 ALTER TABLE "product_contains" ADD CONSTRAINT "product_contains_fk0" FOREIGN KEY ("product_id") REFERENCES "products"("id");
+
+ALTER TABLE "cart" ADD CONSTRAINT "cart_fk0" FOREIGN KEY ("product_id") REFERENCES "products"("id");
+ALTER TABLE "cart" ADD CONSTRAINT "cart_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+
+
+
+
+
+
+
+
+
+
+
