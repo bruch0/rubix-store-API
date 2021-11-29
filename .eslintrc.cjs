3b719cf6730f0fdd469f025812b158d80f862012
bruch0 <lucas.bruch0@gmail.com>
2021-11-29 13:58:39 -0300

    fix: add plugin to eslint adjust for prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
