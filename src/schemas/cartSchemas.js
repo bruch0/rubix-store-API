ee89c96e5473f2577d56e96c940abafdd5ba703c
bruch0 <lucas.bruch0@gmail.com>
2021-11-29 14:02:48 -0300

    refactor: schemas

--- src/schemas/cartSchemas.js
+++ src/schemas/cartSchemas.js
@@ -1,10 +1,10 @@
 import joi from 'joi';
 
-const cartSchema = joi.object({
-  product_id: joi.number().integer().min(1).required(),
-  product_qty: joi.number().integer().min(0).required(),
+const updateCart = joi.object({
+  productId: joi.number().integer().min(1).required(),
+  productQty: joi.number().integer().min(0).required(),
   isUpdate: joi.boolean(),
 });
 
 // eslint-disable-next-line import/prefer-default-export
-export { cartSchema };
+export { updateCart };
